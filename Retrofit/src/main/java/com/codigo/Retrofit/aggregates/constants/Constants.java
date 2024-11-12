0d821a94dd92555641041381691e518b67be41d6
Angielo2772 <angieloet27@gmail.com>
2024-11-12 10:36:45 -0500

    Cambio en Constants

--- Retrofit/src/main/java/com/codigo/Retrofit/aggregates/constants/Constants.java
+++ Retrofit/src/main/java/com/codigo/Retrofit/aggregates/constants/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     public static final String BEARER="Bearer ";
     public static final String BASE_URL = "https://api.apis.net.pe/";
     public static final int REDIS_TTL = 5;
-    public static final String REDIS_KEY_API_SUNAT = "MS:REGISTRO:CONSULTA:SUNAT:";
+    public static final String REDIS_KEY_API_SUNAT = "MS:RETROFIT:CONSULTA:SUNAT:";
 }
